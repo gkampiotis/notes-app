94f26792a03fa4dafbdf13fdf713fb5ed53eb084
Giannis <34453848+gkampiotis@users.noreply.github.com>
2022-05-25 07:51:29 +0300

    backend folder

---
+++ server/index.js
@@ -0,0 +1,67 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+app.use(express.static('build'));
+
+const generateID = () => notes.length > 0 ? Math.max(...notes.map(n => n.id)) + 1 : 0;
+
+
+
+let notes = [
+    { id: 1, content: "HTML is easy", date: "2022-05-30T17:30:31.098Z", important: true },
+    { id: 2, content: "Browser can execute only Javascript", date: "2022-05-30T18:39:34.091Z", important: false },
+    { id: 3, content: "GET and POST are the most important methods of HTTP protocol", date: "2022-05-30T19:20:14.298Z", important: true }
+];
+
+
+
+app.get('/', (request, response) => {
+    response.json(notes);
+});
+
+app.get('/api/notes', (request, response) => {
+    response.json(notes);
+});
+
+app.get('/api/notes/:id', (request, response) => {
+    const noteID = Number(request.params.id);
+    const note = notes.find(note => note.id === noteID);
+
+    if (note) response.json(note);
+    else response.status(404).end();
+});
+
+app.delete('/api/notes/:id', (request, response) => {
+    const noteID = Number(request.params.id);
+    const note = notes.find(note => note.id === noteID);
+
+    response.status(204).end();
+});
+
+app.post('/api/notes', (request, response) => {
+    const body = request.body;
+    if (!body) response.status(400).json({error: "content missing"});
+
+    const note = {
+        id: generateID(),
+        content: body.content,
+        date: new Date(),
+        important: body.important || false
+    };
+    notes.concat(note);
+    
+    response.json(note);
+});
+const unknownEndpoint = (request, response) => response.status(404).send({error: 'unknown endpoint'});
+app.use(unknownEndpoint);
+
+
+
+
+const PORT = process.env.PORT || 2009;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+});
